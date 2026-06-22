0ee559236db59698a9b9a4cad096618a4e061223
Sanya <sanya.24bce10129@vitbhopal.ac.in>
2026-06-22 21:06:45 +0530

    Initial setup

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <main className="p-10">
+        <h2 className="text-3xl font-bold">
+          Welcome to College Discovery Platform
+        </h2>
+      </main>
+    </>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="border-b p-4">
+      <h1 className="text-xl font-bold">
+        College Discovery Platform
+      </h1>
+    </nav>
+  );
+}
